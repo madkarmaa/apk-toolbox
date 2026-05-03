6276fd8c7679cfd579f6411b2fe6516c346b8a05
madkarmaa <madkarma99@gmail.com>
2026-05-03 11:47:41 +0200

    fix: also pipe stdout as not all programs use stderr to show errors

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -19,15 +19,19 @@ pub fn root_dir() -> PathBuf {
 pub fn execute_blocking(program: &str, args: &[&str]) -> io::Result<()> {
     let output = Command::new(program)
         .args(args)
-        .stdout(Stdio::null())
+        .stdout(Stdio::piped())
         .stderr(Stdio::piped())
         .output()?;
 
     if !output.status.success() {
-        let err = String::from_utf8_lossy(&output.stderr);
+        let mut err = String::from_utf8_lossy(&output.stderr);
+        if err.is_empty() {
+            err = String::from_utf8_lossy(&output.stdout);
+        }
+
         return Err(io::Error::new(
             io::ErrorKind::Other,
-            format!("Command failed: {}", err.trim()),
+            format!("{}", err.trim()),
         ));
     }
 

13fcdcb8175e770c747f6dc92d3c02d48148d8c5
madkarmaa <madkarma99@gmail.com>
2026-05-03 23:55:30 +0200

    refactor: minor sintax refactoring

--- src/config/validators.rs
+++ src/config/validators.rs
@@ -1,79 +1,87 @@
+use serde_valid::validation;
 use std::path::Path;
 
-pub fn validate_java_home(path: &Option<String>) -> Result<(), serde_valid::validation::Error> {
+pub fn validate_java_home(path: &Option<String>) -> Result<(), validation::Error> {
     let Some(path) = path else { return Ok(()) };
     let p = Path::new(path);
+
     if !p.exists() {
-        return Err(serde_valid::validation::Error::Custom(
-            "Java home must exist".to_owned(),
+        return Err(validation::Error::Custom(
+            "Java home must exist".to_string(),
         ));
     }
+
     if !p.is_dir() {
-        return Err(serde_valid::validation::Error::Custom(
-            "Java home must be a directory".to_owned(),
+        return Err(validation::Error::Custom(
+            "Java home must be a directory".to_string(),
         ));
     }
+
     if path.ends_with('/') || path.ends_with('\\') {
-        return Err(serde_valid::validation::Error::Custom(
-            "Java home must not have a trailing slash".to_owned(),
+        return Err(validation::Error::Custom(
+            "Java home must not have a trailing slash".to_string(),
         ));
     }
+
     if path.ends_with("/bin") || path.ends_with("\\bin") {
-        return Err(serde_valid::validation::Error::Custom(
-            "Java home must not end in /bin segment".to_owned(),
+        return Err(validation::Error::Custom(
+            "Java home must not end in /bin segment".to_string(),
         ));
     }
+
     Ok(())
 }
 
-pub fn validate_jar_path(path: &Option<String>) -> Result<(), serde_valid::validation::Error> {
+pub fn validate_jar_path(path: &Option<String>) -> Result<(), validation::Error> {
     let Some(path) = path else { return Ok(()) };
     let p = Path::new(path);
+
     if !p.exists() {
-        return Err(serde_valid::validation::Error::Custom(
-            "Path must exist".to_owned(),
-        ));
+        return Err(validation::Error::Custom("Path must exist".to_string()));
     }
+
     if !p.is_file() {
-        return Err(serde_valid::validation::Error::Custom(
-            "Path must be a file".to_owned(),
-        ));
+        return Err(validation::Error::Custom("Path must be a file".to_string()));
     }
+
     if !path.ends_with(".jar") {
-        return Err(serde_valid::validation::Error::Custom(
-            "Path must end in .jar".to_owned(),
+        return Err(validation::Error::Custom(
+            "Path must end in .jar".to_string(),
         ));
     }
+
     Ok(())
 }
 
-pub fn validate_zipalign_path(path: &Option<String>) -> Result<(), serde_valid::validation::Error> {
+pub fn validate_zipalign_path(path: &Option<String>) -> Result<(), validation::Error> {
     let Some(path) = path else { return Ok(()) };
     let p = Path::new(path);
+
     if !p.exists() {
-        return Err(serde_valid::validation::Error::Custom(
-            "Path must exist".to_owned(),
-        ));
+        return Err(validation::Error::Custom("Path must exist".to_string()));
     }
+
     if !p.is_file() {
-        return Err(serde_valid::validation::Error::Custom(
-            "Path must be a file".to_owned(),
-        ));
+        return Err(validation::Error::Custom("Path must be a file".to_string()));
     }
+
     if cfg!(windows) && !path.ends_with(".exe") {
-        return Err(serde_valid::validation::Error::Custom(
-            "Path must end in .exe on Windows".to_owned(),
+        return Err(validation::Error::Custom(
+            "Path must end in .exe on Windows".to_string(),
         ));
     }
+
     Ok(())
 }
 
-pub fn validate_keystore_path(path: &Option<String>) -> Result<(), serde_valid::validation::Error> {
+pub fn validate_keystore_path(path: &Option<String>) -> Result<(), validation::Error> {
     let Some(path) = path else { return Ok(()) };
+
     if !path.ends_with(".jks") {
-        return Err(serde_valid::validation::Error::Custom(
-            "Path must end in .jks".to_owned(),
+        return Err(validation::Error::Custom(
+            "Path must end in .jks".to_string(),
         ));
     }
+
     Ok(())
 }
